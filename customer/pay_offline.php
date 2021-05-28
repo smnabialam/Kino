96f534e36f21ed84d2fcc435b4392fc5bd6e2027
S M NABI ALAM <sahilalam611@gmail.com>
2021-05-28 21:08:45 +0600

    Initial commit

---
+++ customer/pay_offline.php
@@ -0,0 +1,41 @@
+<center><!--  center Begin  -->
+    
+    <h1> Pay Offline </h1>
+    
+    <p class="text-muted">
+        
+        If you have any questions, feel free to <a href="../contact.php">Contact Us</a>. Our Customer Service work <strong>24/7</strong>
+        
+    </p>
+    
+</center><!--  center Finish  -->
+
+
+<hr>
+
+
+<div class="table-responsive"><!--  table-responsive Begin  -->
+    
+    <table class="table table-bordered table-hover table-striped"><!--  table table-bordered table-hover Begin  -->
+        
+        <thead><!--  thead Begin  -->
+            
+            <tr><!--  tr Begin  -->
+            
+                <th> Bank Account Details: </th>
+                
+
+            </tr><!--  tr Finish  -->
+            
+        </thead><!--  thead Finish  -->
+        
+        <tbody><!--  tbody Begin  -->
+           
+           <td> Bank Name: Standard Chartered Bank | Account No: 121-321-892 | Branch Name: Dhaka | Branch Code: 215261247 </td>
+    
+            
+        </tbody><!--  tbody Finish  -->
+        
+    </table><!--  table table-bordered table-hover Finish  -->
+    
+</div><!--  table-responsive Finish  -->
